2b6cdf2a4c145607efde1038fbf3799e63de4eed
Maximilian Köhl <koehl@silitics.com>
2026-02-24 16:18:40 +0100

    support for custom mirrors
    https://github.com/rugix/rugix/pull/92

--- container/repositories/core/recipes/debian-bootstrap/steps/00-run.sh
+++ container/repositories/core/recipes/debian-bootstrap/steps/00-run.sh
@@ -20,20 +20,23 @@ case "${RUGIX_ARCH}" in
         exit 1
 esac
 
-if [ "${RECIPE_PARAM_SNAPSHOT}" != "" ]; then 
-    mmdebstrap \
-        --skip=check/qemu \
-        --architectures="${DEBIAN_ARCH}" \
-        --aptopt='Acquire::Check-Valid-Until "false"' \
-        --aptopt='Apt::Key::gpgvcommand "/usr/libexec/mmdebstrap/gpgvnoexpkeysig"' \
-        --include="ca-certificates mmdebstrap" \
-        "${RECIPE_PARAM_SUITE}" \
-        "${RUGIX_ROOT_DIR}" \
-        "https://snapshot.debian.org/archive/debian/${RECIPE_PARAM_SNAPSHOT}/"
-else
-    mmdebstrap \
-        --skip=check/qemu \
-        --architectures="${DEBIAN_ARCH}" \
-        "${RECIPE_PARAM_SUITE}" \
-        "${RUGIX_ROOT_DIR}"
+OPTS=(
+    "--skip=check/qemu"
+    "--architectures=${DEBIAN_ARCH}"
+)
+TARGET_MIRROR=""
+
+if [ -n "${RECIPE_PARAM_SNAPSHOT}" ]; then
+    TARGET_MIRROR="https://snapshot.debian.org/archive/debian/${RECIPE_PARAM_SNAPSHOT}/"
+    OPTS+=("--aptopt=Acquire::Check-Valid-Until=false")
+    OPTS+=("--aptopt=Apt::Key::gpgvcommand=/usr/libexec/mmdebstrap/gpgvnoexpkeysig")
+    OPTS+=("--include=ca-certificates,mmdebstrap")
+elif [ -n "${RECIPE_PARAM_MIRROR}" ]; then
+    TARGET_MIRROR="deb [trusted=yes] ${RECIPE_PARAM_MIRROR} ${RECIPE_PARAM_SUITE} main"
 fi
+
+mmdebstrap \
+    "${OPTS[@]}" \
+    "${RECIPE_PARAM_SUITE}" \
+    "${RUGIX_ROOT_DIR}" \
+    ${TARGET_MIRROR:+"$TARGET_MIRROR"}
