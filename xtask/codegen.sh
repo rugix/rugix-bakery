763aa21724677e6b8d060ef76031a61b1ebb1c06
Maximilian Köhl <koehl@silitics.com>
2026-04-13 18:55:18 +0200

    update schemas

--- xtask/codegen.sh
+++ xtask/codegen.sh
@@ -7,14 +7,4 @@ sidex generate rust src/config/generated
 ./generate-json-schema.sh
 popd
 
-pushd crates/apps/rugix-ctrl
-sidex generate rust src/config/generated
-./generate-json-schema.sh
-popd
-
-pushd crates/libs/rugix-bundle
-sidex generate rust src/manifest/generated
-./generate-json-schema.sh
-popd
-
-cargo +nightly fmt
+cargo fmt
